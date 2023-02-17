10dfcc0efcb94e5c2e5d379689e4d433eef7a797
jacob <ysuhiyini72@gmail.com>
2023-02-17 15:48:26 -0800

    task finished successfully

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int y;
+for (y = '0'; y <= '9'; y++)
+putchar(y);
+putchar('\n');
+return (0);
+}
