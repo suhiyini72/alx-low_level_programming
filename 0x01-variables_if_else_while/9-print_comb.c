6ce395badca10c29eeb7ec14815fc5f3421f14bf
jacob <ysuhiyini72@gmail.com>
2023-02-17 16:14:14 -0800

    another commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int y;
+for (y = '0'; y <= '9'; y++)
+{
+putchar(y);
+if (y != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
