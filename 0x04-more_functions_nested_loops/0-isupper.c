de3b2a6034d7a529b2cce4dd969e0441beb5d934
jacob <ysuhiyini72@gmail.com>
2023-02-23 17:04:42 -0800

    more functions

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
 * _isupper - checks for uppercase character
-* @c: Variable text
+* 
 * Return: Always 0.
 */
+
 int _isupper(int c)
 {
-
- if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
  {
- return (1);
+	 return (1);
  }
- else
+	 else
 {
- return (0);
+	return (0);
 }
 }
