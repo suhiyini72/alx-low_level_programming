f0cca56b3a3d42403acaa119a30d85a065fb1306
jacob <ysuhiyini72@gmail.com>
2023-02-28 06:19:32 -0800

    string function added

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen -  a function that calculates the length of a string
+ * @s: the input string
+ * Return: returns the number of bytes of the string s
+ */
+int _strlen(char *s)
+{
+	int str_length;
+
+	while (s[str_length] != '\0')
+		str_length++;
+
+	return (str_length);
+}
