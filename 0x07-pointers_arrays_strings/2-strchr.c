5736bd995ece872278ef6e720f231e637cf8776b
jacob <ysuhiyini72@gmail.com>
2023-03-07 09:24:48 -0800

    more pointers added now

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - The main entry point
+ * @s: first input
+ * @c: second input
+ * Return: Always 0 (Success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int k = 0;
+
+	for (; s[k] >= '\0'; k++)
+	{
+		if (s[k] == c)
+			return (&s[k]);
+	}
+	return (0);
+}
+
