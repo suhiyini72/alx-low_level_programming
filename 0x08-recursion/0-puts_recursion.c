6389036263f44215df7e336b13c33a15e79fd621
jacob <ysuhiyini72@gmail.com>
2023-03-09 14:03:17 -0800

    recursive task done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - a function that prints a string, followed by a new line
+ * @s: An input string to be printed
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
