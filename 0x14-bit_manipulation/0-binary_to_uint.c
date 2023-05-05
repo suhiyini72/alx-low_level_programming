d01437082fd0c3339e8b7598b34a5736165032ff
jacob <ysuhiyini72@gmail.com>
2023-05-05 15:03:55 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,13 @@ int k;
 unsigned int dval = 0;
 
 if (!b)
-	return (0);
+return (0);
 
 for (k = 0; b[k]; k++)
 {
-	if (b[k] < '0' || b[k] > '1')
-		return (0);
-		dval = 2 * dval + (b[k] - '0');
+if (b[k] < '0' || b[k] > '1')
+return (0);
+dval = 2 * dval + (b[k] - '0');
 }
 return (dval);
 }
