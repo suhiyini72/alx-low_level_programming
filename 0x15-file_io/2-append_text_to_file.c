13c787f26d456b6ca1ad6409f1a04ac9ef6f2fc2
jacob <ysuhiyini72@gmail.com>
2023-05-10 10:39:09 -0700

    updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,8 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <fcntl.h>
 #include "main.h"
 
 /**
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,10 @@
-#include "main.h"
-#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int s_klose(int);
 
