e5da6104d225de49fb79703f3f76dda1aa8abf7e
suhiyini <ysuhiyini72@gmail.com>
2023-07-06 17:00:52 -0700

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,10 +12,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 int bval;
 
-if (dx > 63)
+if (index > 63)
 return (-1);
 
-bval = (n >> dx) & 1;
+bval = (n >> index) & 1;
 
 return (bval);
 }
